032b3423a81c82cd272a61fdd5d43715dbdd3589
Pleexus <prajwalbisht9999@gmail.com>
2022-04-21 22:03:33 +0530

    added catalog level 2

--- SpringMicro1/rating-data-service/src/main/java/io/microservice/ratingdataservice/resource/RatingsResource.java
+++ SpringMicro1/rating-data-service/src/main/java/io/microservice/ratingdataservice/resource/RatingsResource.java
@@ -22,8 +22,8 @@ public class RatingsResource {
 	@RequestMapping("users/{userId}")
 	public UserRating getUserRating(@PathVariable("userId") String userId ) {
 		List <Rating> ratings = Arrays.asList(
-				new Rating("1234", 4),
-				new Rating("5678", 3)
+				new Rating("2", 4),
+				new Rating("550", 3)
 		);
 
 		UserRating userRating = new UserRating();
